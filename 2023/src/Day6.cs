ec104e5813fb9a5550c6522637eb2e712a726d2d
Andrew Best <andrew.best@octopus.com>
2023-12-08 08:38:49 +1030

    Day 6 part 1

---
+++ 2023/src/Day6.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace AdventOfCode2023;
+
+public class Day6
+{
+
+    [Fact]
+    void Main()
+    {
+        var inputs = new (int time, int distance)[] {
+            (63, 411),
+            (78, 1274),
+            (94, 2047),
+            (68, 1035)
+        };
+
+        int GetPossibilities(int distance, int time)
+        {
+            for (var i = 0; i < time; i++)
+            {
+                var timeRemaining = time - i;
+
+                if (timeRemaining * i > distance)
+                {
+                    return timeRemaining - i + 1;
+                }
+            }
+
+            return 0;
+        }
+        
+        var result = inputs.Aggregate(1, (acc, curr) => acc * GetPossibilities(curr.distance, curr.time));
+        
+        Assert.Equal(781200, result);
+    }
+}
